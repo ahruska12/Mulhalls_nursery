d45f9359ec434d2c3626934a62b823136065d338
ahruska12 <112120014+ahruska12@users.noreply.github.com>
2024-02-21 14:56:04 -0600

    fixed

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-from user import views
+from .views import RegisterView
 
 app_name = "user"
 
 urlpatterns = [
     # Url that directs to the register view
-    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
     ]
--- user/views.py
+++ user/views.py
@@ -3,7 +3,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_jwt.serializers import User
 
-from user.serializer import RegisterSerializer
+from .serializer import RegisterSerializer
+
 
 # generic view for registering to our site
 
@@ -11,4 +12,3 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
-
