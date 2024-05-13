8a4369d8d9d619f3a2026ac7fb424cb74718d9b8
ahruska12 <112120014+ahruska12@users.noreply.github.com>
2024-05-13 15:58:31 -0500

    working on deploy

--- mulhalls_nursery/settings.py
+++ mulhalls_nursery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3kx603#!!et)md@z!o^wh)tzlfz)+5yjxsugms4sieqcgi=gy0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
